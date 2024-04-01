2c1dcbd0399ee5529e874beb8fa2039726a998db
friesenkatharina <friesen.katharina2022@gmail.com>
2024-04-01 17:18:03 +0200

    add loginForm, install backap packages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,13 +2,7 @@ function About() {
   return (
     <>
       <div className="block rounded-lg bg-white shadow-secondary-1 dark:bg-surface-dark dark:text-white text-surface">
-        <div className="relative overflow-hidden bg-cover bg-no-repeat">
-          <img
-            className="rounded-t-lg"
-            src="https://tecdn.b-cdn.net/img/new/slides/041.webp"
-            alt=""
-          />
-        </div>
+        <div className="relative overflow-hidden bg-cover bg-no-repeat"></div>
         <div className="p-6">
           <h5 className="mb-2 text-xl font-medium leading-tight">Card title</h5>
           <p className="mb-4 text-base">
@@ -32,13 +26,7 @@ function About() {
             <small>Last updated 3 mins ago</small>
           </p>
         </div>
-        <div className="relative overflow-hidden bg-cover bg-no-repeat">
-          <img
-            className="rounded-b-lg"
-            src="https://tecdn.b-cdn.net/img/new/slides/042.webp"
-            alt=""
-          />
-        </div>
+        <div className="relative overflow-hidden bg-cover bg-no-repeat"></div>
       </div>
     </>
   );
