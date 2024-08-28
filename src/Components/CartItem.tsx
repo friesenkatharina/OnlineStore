b465da053e41a27fa070c33eed7b18bd5c2d9b78
friesenkatharina <friesen.katharina2022@gmail.com>
2024-08-28 09:52:43 +0200

    chore: Remove unused CartItemProps in CartItem component

--- src/Components/CartItem.tsx
+++ src/Components/CartItem.tsx
@@ -2,10 +2,7 @@ import { Button, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import storeItems from "../items.json";
 import { formatCurrency } from "../utilities/formatCurrency";
-
-type CartItemProps = {
-  id: number;
-  quantity: number;
+;
 };
 
 export function CartItem({ id, quantity }: CartItemProps) {
