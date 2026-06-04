68c811c86342e16fcc7d162891c2c4dfd52ce369
friesenkatharina <friesen.katharina2022@gmail.com>
2026-06-04 13:45:47 +0200

    chore: remove unused backend folder, replaced by serverless api/

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-// In this file we will configure the environment variables and other configurations for the application.
-// Benefits: we can use the configurations in different files and we can change the configurations in one place.
-// import dotenv from "dotenv";
-// dotenv.config();
